492a68053fe2492bcc91f45ae63f418a1a834fd7
Amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-06-20 16:34:50 +0330

    update to part 11

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ intro
 create
 explain mvt
 superuser
+app
+templates
+filters tag
+models
+reading Model
+extends
+bootstrap
+detail
--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('', views.home),
+    path('detail/<int:todo_id>/', views.detail),
 ]
